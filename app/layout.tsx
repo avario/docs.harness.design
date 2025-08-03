ee891150af0ee18ec8ee732deee6e9887bd91f0b
Avario <avario@hotmail.com>
2025-08-03 20:36:14 +1200

    Font and logo link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import Logo from "./Logo";
 import { Analytics } from "@vercel/analytics/next";
+import "./styles.css";
 
 export const metadata = {
   // Define your metadata here
@@ -13,6 +14,7 @@ export const metadata = {
 const navbar = (
   <Navbar
     logo={<Logo />}
+    logoLink="https://harness.design"
     // ... Your additional navbar options
   />
 );
@@ -23,11 +25,18 @@ const footer = (
 export default async function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head
-      // ... Your additional head options
-      >
+      <Head>
         <link rel="shortcut icon" href="/images/general/icon.svg" />
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Layout
