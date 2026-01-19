ca3833674a6fe03e8bf24f7aa7b68e96b4311914
Samuel Blok <70375123+anono-mouse@users.noreply.github.com>
2026-01-19 17:47:40 +1300

    Privacy Policy and TOS

--- content/_meta.js
+++ content/_meta.js
@@ -1,28 +1,12 @@
 export default {
-  index: '',
-  'quick-start': {
-    theme: {
-      collapsed: true
-    }
-  },
-  components: {
-    theme: {
-      collapsed: true
-    }
-  },
-  parts: {
-    theme: {
-      collapsed: true
-    }
-  },
-  features: {
-    theme: {
-      collapsed: true
-    }
-  },
-  examples: {
-    theme: {
-      collapsed: true
-    }
-  }
+  index: {},
+  'quick-start': { theme: { collapsed: true } },
+  components: { theme: { collapsed: true } },
+  parts: { theme: { collapsed: true } },
+  features: { theme: { collapsed: true } },
+  examples: { theme: { collapsed: true } },
+  preferences: {},
+  shortcuts: {},
+  updates: {},
+  legal: { theme: { collapsed: true } }
 }
---
+++ content/legal/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  'privacy-policy': '',
+  'terms-of-service': '',
+}
