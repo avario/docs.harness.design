ce67e39f33f78e92a2b5a3d6500c827831371214
Samuel Blok <70375123+anono-mouse@users.noreply.github.com>
2026-04-10 12:34:05 +1200

    Added Pro Plan

--- content/_meta.tsx
+++ content/_meta.tsx
@@ -1,3 +1,14 @@
+export function IconPro({ size = "1.5em", className = "" }) {
+  return (
+    <img
+      src="/images/general/pro.svg"
+      style={{ display: "inline", verticalAlign: "middle", width: size, height: size, marginRight: "0.5em", marginBottom: "0.3em" }}
+      className={className}
+      alt="Pro"
+    />
+  );
+}
+
 export default {
   index: {},
   'quick-start': { theme: { collapsed: true } },
@@ -12,4 +23,5 @@ export default {
   legal: { theme: { collapsed: true } },
   about: { },
   'beta-end': { title: <span style={{ color: '#F49F00' }}>Beta has Ended</span> },
+  'pro-plan': { title: <span style={{ color: '#F49F00' }}><IconPro/>Pro Plan</span> },
 }
