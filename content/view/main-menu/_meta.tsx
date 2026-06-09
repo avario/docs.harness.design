7619279155016ea1d6255f016aecca434e1dab79
Avario <avario@hotmail.com>
2026-06-09 17:01:20 +1200

    Updates before formboard release

--- content/view/main-menu/_meta.tsx
+++ content/view/main-menu/_meta.tsx
@@ -1,9 +1,8 @@
-import { IconFolder, IconFileExport, IconShare2, IconSettings, IconUserCircle } from "@tabler/icons-react";
+import { IconFolder, IconFileExport, IconShare2, IconSettings } from "@tabler/icons-react";
 
 export default {
   "open": { title: <><IconFolder className="tabler-icon"/> Open Harness</> },
   "export": { title: <><IconFileExport className="tabler-icon"/> Export Options</> },
   "share": { title: <><IconShare2 className="tabler-icon"/> Share Harness</> },
   "settings": { title: <><IconSettings className="tabler-icon"/> Settings</> },
-  "account": { title: <><IconUserCircle className="tabler-icon"/> Account</> },
 };
